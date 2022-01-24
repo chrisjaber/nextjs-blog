4e6d6a87ae7e5a3a0852e02c2c18c8e668e0b45a
Chris Jaber <c@jabr.co>
2022-01-24 16:53:03 -0600

    finished into project

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+// pages/404.js
+import utilStyles from '../styles/utils.module.css'
+import Layout from '../components/layout'
+
+export default function Custom404() {
+    return <Layout>
+<h1 className={utilStyles.headingXl}>404 - Page Not Found</h1>
+</Layout>
+}
